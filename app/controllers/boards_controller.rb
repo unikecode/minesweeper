fca4fcdea32702d2805e3bffc916f4abd73029b1
unikecode <talksabeen@gmail.com>
2023-08-04 15:15:08 -0700

    Created Board Controller and define Root URL

---
+++ app/controllers/boards_controller.rb
@@ -0,0 +1,25 @@
+class BoardsController < ApplicationController
+  def new
+    @board = Board.new
+  end
+
+  def create
+    @board = Board.new(board_params)
+    binding.pry
+    if @board.save
+      redirect_to board_path(@board)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @board = Board.find_by_id(params[:id])
+  end
+
+  private
+
+  def board_params
+    params.require(:board).permit(:email, :width, :height, :mines_count, :name)
+  end
+end
